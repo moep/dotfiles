0594790fe72bac43a5d1fa63776716d479334c0d
Karsten Groll <moep@users.noreply.github.com>
2020-10-29 22:25:28 +0100

    Fixed "bold" not being declared correctly

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,4 +1,2 @@
-set LS_COLORS dxfxcxdxbxegedabagacad
 alias ls 'ls -lhs' 
 alias .. 'cd ..'
-alias jrnl '~/.local/bin/jrnl'
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_hide_untrackedfiles 1
 
-set -g __fish_git_prompt_color_branch white bold
+set -g __fish_git_prompt_color_branch white --bold
 set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
@@ -12,7 +12,7 @@ set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green bold
+set -g __fish_git_prompt_color_cleanstate green --bold
 
 set -g __fish_git_prompt_char_dirtystate "*"
 set -g __fish_git_prompt_char_invalidstate "#" 
